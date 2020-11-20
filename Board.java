2a886df0140c91a4e0f0347e5ea3128a46b2c243
BriMarieRod <briannarodriguez0529@gmail.com>
2020-11-20 13:47:58 -0600

    Allow both black and white pieces to be drawn

--- Board.java
+++ Board.java
@@ -36,12 +36,18 @@ public class Board {
 			for(int j = i%2; j < 8; j+=2)
 				g.fillRect(j*tileSize+posX, i*tileSize+posY, tileSize, tileSize);
 
-		g.setColor(LIGHT_PIECE);
 		g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, tileSize));
+		//g.setColor(LIGHT_PIECE);
 		for(int i = 0; i < 8; i++)
 			for(int j = 0; j < 8; j++)
-				if(pieces[i][j] != null && pieces[i][j].isWhite)
+				if(pieces[i][j] != null) {
+					if(pieces[i][j].isWhite)
+						g.setColor(LIGHT_PIECE);
+					else
+						g.setColor(DARK_PIECE);
 					pieces[i][j].draw(g, posX + tileSize*i, posY + tileSize*j + tileSize);
+				}
+
 		//pieces[4][7].draw(g, posX + tileSize*4, posY + tileSize*7 + tileSize);
 		//System.out.println(posX + tileSize*4);
 

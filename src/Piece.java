153611735c87b1f2a77336f012599347ba250a2d
BriMarieRod <briannarodriguez0529@gmail.com>
2020-11-30 10:58:07 -0600

    Add possibleMoves as an abstract class

--- src/Piece.java
+++ src/Piece.java
@@ -1,4 +1,6 @@
 import java.awt.Graphics;
+import java.util.Set;
+import java.util.List;
 
 public abstract class Piece {
 	
@@ -11,10 +13,12 @@ public abstract class Piece {
 
 		this.isWhite = isWhite;
 		this.icon = icon;
-		
+
 		isSelected = false;
 
 	}
+
+	abstract Set<List<Integer>> possibleMoves(Board board, int posX, int posY);
 
 	abstract void draw(Graphics g, int x, int y);
 
